59a1d06471026a7c46922591132c5bddc1b7ef30
Jordan Doig <jordandoig@live.com>
2017-06-05 15:15:45 -0600

    Error!

---
+++ GrooveBass/App/Components/Main.js
@@ -0,0 +1,26 @@
+// var React = require('react-native');
+import React, { Component } from 'react';
+var ReactNative = require('react-native');
+// var api = require('../Utils/api');
+// var Dashboard = require('./Dashboard');
+
+var {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicatorIOS
+} = ReactNative;
+
+class Main extends React.Component{
+  render(){
+    return (
+      <View>
+        <Text>Testing our SHIT</Text>
+      </View>
+    )
+  }
+};
+
+module.exports = Main;
--- GrooveBass/index.ios.js
+++ GrooveBass/index.ios.js
@@ -1,53 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+var Main = require('./App/Components/Main');
+var ReactNative = require('react-native');
 
-export default class GrooveBass extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} = ReactNative;
 
-const styles = StyleSheet.create({
-  container: {
+import {StackNavigator} from 'react-navigation';
+
+var styles = StyleSheet.create({
+  container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111'
   },
 });
 
-AppRegistry.registerComponent('GrooveBass', () => GrooveBass);
+// var GrooveBass = React.createClass({
+class GrooveBass extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  }
+  render() {
+    return (
+      <Text>Nav workin</Text>
+    );
+  }
+  const Groovin = StackNavigator({
+    Home: {screen:GrooveBass}
+  })
+});
+AppRegistry.registerComponent('Groovin', () => Groovin);
