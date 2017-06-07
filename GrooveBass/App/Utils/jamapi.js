8f424e425de40564b0ecfe091ff0282111f03e78
Michelle Bergquist <caker25@gmail.com>
2017-06-06 19:02:34 -0600

    Working jambase api

--- GrooveBass/App/Utils/jamapi.js
+++ GrooveBass/App/Utils/jamapi.js
@@ -1,8 +1,7 @@
 var jamapi = {
-  getMusic(location){
+  getMusic (location) {
     var url = 'http://api.jambase.com/events?zipCode=80202&api_key=mnzs2h3am23p7gaqkfh46q5a'
-    // var url = `https://api.github.com/users/mihshhehl`
-    return fetch(url).then((res) => res.json())
+    return fetch(url).then(res => res.json())
   }
 }
-module.exports = jamapi;
+module.exports = jamapi
